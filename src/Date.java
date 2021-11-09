30e377b32a8a73924f5bc802cb11af58e3a6dba9
trucher <61274375+laruchsix@users.noreply.github.com>
2021-11-09 19:22:37 +0100

    isValidDate
    Coucou

--- src/Date.java
+++ src/Date.java
@@ -15,6 +15,48 @@ public class Date implements Serializable {
         this.year = year;
     }
 
+    public static boolean isBissextile (int month)
+    {
+        if ((month % 4) == 0))
+        {
+            if ((month % 100) == 0)
+            {
+                return (month % 100) == 0;
+            }
+            else
+                return true;
+        }
+        else 
+            return false;
+    }
+
+    public static boolean isValidDate(int year, int day, int month)
+    {
+        boolean ret = true;
+
+        ret = (month > 0 && month < 13);
+
+        if (ret)
+        {
+            if ( month == 2)
+            {
+                if (isBissextile(month))
+                    ret = (month == 29);
+                else
+                    ret = (month == 28);
+            }
+            else if ((month % 2 == 1 && month <= 8) || (month % 2 == 0 && month >= 8))
+            {
+                ret = (month == 31);
+            }
+            else
+            {
+                ret = (month == 30);
+            }
+        }
+        return ret;
+    }
+
     @Override
     public String toString()
     {
