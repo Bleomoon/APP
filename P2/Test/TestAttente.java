471b8afd5c6865c207672b86cc3e056e5996e9e9
AnanthTrebuh <xignir@gmail.com>
2021-11-22 22:07:58 +0100

    test attente

---
+++ P2/Test/TestAttente.java
@@ -0,0 +1,34 @@
+package Test;
+
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import src.*;
+import src.Client;
+
+
+/*
+** Création de un client qui fait plusieurs requete
+*/
+public class TestAttente {
+    public static void main(String[] args) throws RemoteException {
+        ClockDist objdist = null;
+		ClientInt objserv = null;
+
+        try {
+            System.out.println("Creation de l'objet.");
+            objserv=new Client();
+            System.out.println("Enregistrement de l'objet.");
+            Naming.rebind("client",objserv);
+            System.out.println("serveur operationnel.");
+
+            System.out.println("Searching for object.");
+            String url = "rmi://" + args[0] + "/echoservice";
+            objdist = (ClockDist) Naming.lookup(url);
+
+            int id1 = ((Client)objserv).connectNew(4, 500, "localhost", objdist);
+        }
+        catch ( Exception e) {
+            System.out.println(e);
+        }
+    }
+}
